78552b755333575fefc07e475f93eda178313fe6
Shivamyadav1902 <helloshivam1902@gmail.com>
2025-05-25 01:40:44 +0530

    cusreport.java

--- cusreport.java
+++ cusreport.java
@@ -13,10 +13,7 @@ import java.sql.ResultSetMetaData;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author parth
- */
+
 public class cusreport extends javax.swing.JInternalFrame {
 
     /**
